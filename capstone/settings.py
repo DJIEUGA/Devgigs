0dbd37ae2db2f03692d42fc556fa23c1a8a67701
djieugaryle <93140753+DJIEUGA@users.noreply.github.com>
2023-09-20 18:10:27 +0100

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-++8aw#3y4z#9q6+^hp-nn_rm&+u0dm(#ka8uuy#^=s-83@k-@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
