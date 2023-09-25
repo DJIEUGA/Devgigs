b84c6819067aa37c49f1c857179f49d89a306692
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2023-09-25 20:08:41 +0100

    updated settings.py

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py makemigrations devgigs
-python3.9 manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
