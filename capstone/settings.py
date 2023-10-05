34c41aebd2dad94b17f1d314aa3f2946b42a04be
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2023-10-05 02:45:28 +0100

    Set debug to False

--- capstone/settings.py
+++ capstone/settings.py
@@ -27,10 +27,8 @@ print(os.environ.get("DEVELOPMENT_DB_URL"))
 SECRET_KEY = 'django-insecure-++8aw#3y4z#9q6+^hp-nn_rm&+u0dm(#ka8uuy#^=s-83@k-@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get("VERCEL_ENV") == "production":
-    DEBUG = True
-else:
-    DEBUG = False
+
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
